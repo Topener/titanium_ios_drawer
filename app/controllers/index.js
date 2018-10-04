b897df9ea0698505470171b9db880467d9614aff
Rene Pot <rene@topener.nl>
2018-10-04 12:30:42 +0200

    add ability to swipe in the menu

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,43 @@
 $.getView().open();
 
 function showMenu(e){
-  Alloy.createController('menu', {handleNavigation: navigate, direction: e.source.direction}).getView().open();
+  var direction;
+  if (e.hasOwnProperty('source')){
+    direction = e.source.direction;
+  } else if (e.direction){
+    direction = e.direction;
+  }
+  Alloy.createController('menu', {handleNavigation: navigate, direction: direction}).getView().open();
 }
 
 function navigate(controller, args){
   $.navwin.openWindow(Alloy.createController(controller, args).getView());
+}
+
+var startX = false;
+var swipeInDirection;
+function handleTouchstart(e){
+  var threshold = 30;
+  if (e.x < threshold){
+    startX = e.x;
+    swipeInDirection = 'left';
+  } else if (Ti.Platform.displayCaps.platformWidth - threshold < e.x){
+    swipeInDirection = 'right';
+    startX = e.x;
+  }
+}
+
+function handleTouchmove(e){
+  if (startX === false) return;
+  
+  if (swipeInDirection === 'left' && e.x - startX > 50){
+    startX = false;
+    showMenu({direction: 'left'});
+    swipeInDirection = false;
+  }
+  if (swipeInDirection === 'right' && startX - e.x > 50){
+    startX = false;
+    showMenu({direction: 'right'});
+    swipeInDirection = false;
+  }
 }
